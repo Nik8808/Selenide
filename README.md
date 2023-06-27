d39d78bf47d0b234b8e47859bfa425173d3832f4
Nikita <inik8808@gmail.com>
2023-06-28 01:00:14 +0500

    Completion of task one

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/4hwu22l9yne8m9wp?svg=true)](https://ci.appveyor.com/project/Nik8808/selenide)
--- src/test/java/ru/netology/service/DeliveryCardTest.java
+++ src/test/java/ru/netology/service/DeliveryCardTest.java
@@ -1,47 +1,58 @@
 package ru.netology.service;
 
-import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeliveryCardTest {
-    LocalDate date = new LocalDate();
+    private String getFutureDate(int addDays, String pattern) {
+
+        String formattedDate = LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+        return formattedDate;
+    }
+
+    String planningDate = getFutureDate(4, "dd.MM.yyyy");
+    String planningDateDay = getFutureDate(4, "d");
+
     @Test
-    void cardTest() throws InterruptedException {
+    void cardTest() {
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id=date] input").doubleClick().setValue(planningDate);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Брэт Пит");
         $("[data-test-id=phone] input").setValue("+12345678901");
         $("[data-test-id=agreement]").click();
         $(".button").click();
         $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(11));
-        Thread.sleep(2000);
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 
     @Test
-    void cardTestChoice() throws InterruptedException {
+    void cardTestChoice() throws InterruptedException{
         open("http://localhost:9999/");
         $("[data-test-id=city] input").setValue("Кр");
         $(byText("Краснодар")).click();
         $("[data-test-id='date']").click();
-        $("[data-step='1']").click();
-        $("[data-day='1690657200000']").click();
+        $(byText(planningDateDay)).click();
         Thread.sleep(2000);
         $("[data-test-id=name] input").setValue("Брэт Пит");
         $("[data-test-id=phone] input").setValue("+12345678901");
         $("[data-test-id=agreement]").click();
         $(".button").click();
         $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(11));
+        $(".notification__content").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15)).shouldBe(Condition.visible);
     }
 
 }
